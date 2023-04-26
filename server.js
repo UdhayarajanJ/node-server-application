b0e228ff8bcfe29c68cb047127c9796629f25112
UdhayarajanJ <udhayarajan080@gmail.com>
2023-04-26 14:28:36 +0530

    to run the express api server

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log('Udhayarajan J is a mean stack developer');
+const express = require('express');
+
+const app = new express();
+
+const port = 5000;
+
+app.listen(port, () => {
+    console.log(`Server Running On Port ${port}`);
+});
