589ad6f9c6b6c0c4634229c19fc3e665606ab758
UdhayarajanJ <udhayarajan080@gmail.com>
2023-04-26 14:38:16 +0530

    To implement the dotenv and config the port

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+// Import Package
 const express = require('express');
+const env = require('dotenv').config();
 
+//Initialize the server configuration
 const app = new express();
-
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server Running On Port ${port}`);
