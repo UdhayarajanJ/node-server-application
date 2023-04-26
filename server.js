02e42f63026c44b974e685183011c2069a3db020
UdhayarajanJ <udhayarajan080@gmail.com>
2023-04-26 14:41:58 +0530

    Just develop and check simple api for contact

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const env = require('dotenv').config();
 const app = new express();
 const port = process.env.PORT || 5000;
 
+app.get('/api/contact', (req, res) => {
+    res.send("Get All Contacts");
+});
+
 app.listen(port, () => {
     console.log(`Server Running On Port ${port}`);
 });
