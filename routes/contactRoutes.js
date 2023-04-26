4d3ba4b9a2041f7d7674950aba30375189a6a36e
UdhayarajanJ <udhayarajan080@gmail.com>
2023-04-26 15:25:42 +0530

    To defined the all contact routes

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -5,5 +5,21 @@ router.route('/').get((req, res) => {
     res.status(201).json({ message: "Get All Contacts" });
 });
 
+router.route('/').post((req, res) => {
+    res.status(201).json({ message: "Create Contacts" });
+});
+
+router.route('/:id').put((req, res) => {
+    res.status(201).json({ message: `Update Contacts For ${req.params.id}` });
+});
+
+router.route('/:id').get((req, res) => {
+    res.status(201).json({ message: `Get Contact For ${req.params.id}` });
+});
+
+router.route('/:id').delete((req, res) => {
+    res.status(201).json({ message: `Delete Contact For ${req.params.id}` });
+});
+
 
 module.exports = router;
