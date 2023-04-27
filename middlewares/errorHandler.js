b08bb81fef386e6644aff0eeb39c71bbcf94437e
UdhayarajanJ <udhayarajan080@gmail.com>
2023-04-27 09:55:05 +0530

    To make the error handler globally

---
+++ middlewares/errorHandler.js
@@ -0,0 +1,46 @@
+const { constants } = require('../constants');
+const errorHandler = (err, req, res, next) => {
+    const statusCode = res.statusCode ? res.statusCode : 500;
+    switch (statusCode) {
+        case constants.VALIDATION_ERROR:
+            res.json({
+                title: "Validatation Failed",
+                message: err.message,
+                stackTrace: err.stack
+            });
+            break;
+        case constants.NOT_FOUND:
+            res.json({
+                title: "Not Found",
+                message: err.message,
+                stackTrace: err.stack
+            });
+            break;
+        case constants.UNAUTHORIZED:
+            res.json({
+                title: "Unauthorized",
+                message: err.message,
+                stackTrace: err.stack
+            });
+            break;
+        case constants.FORBIDDEN:
+            res.json({
+                title: "Forbidden",
+                message: err.message,
+                stackTrace: err.stack
+            });
+            break;
+        case constants.SERVER_ERROR:
+            res.json({
+                title: "Server Error",
+                message: err.message,
+                stackTrace: err.stack
+            });
+            break;
+        default:
+            console.log('No Error All Good');
+            break;
+    }
+};
+
+module.exports = errorHandler;
