b8908c01454af6fda608be212e71db4b3dc0951b
UdhayarajanJ <udhayarajan080@gmail.com>
2023-04-27 09:56:57 +0530

    to configure the route of the application

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,25 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const { getContact, createContact, updateContact, deleteContact, getContactById } = require('../controllers/contactController');
 
-router.route('/').get((req, res) => {
-    res.status(201).json({ message: "Get All Contacts" });
-});
-
-router.route('/').post((req, res) => {
-    res.status(201).json({ message: "Create Contacts" });
-});
-
-router.route('/:id').put((req, res) => {
-    res.status(201).json({ message: `Update Contacts For ${req.params.id}` });
-});
-
-router.route('/:id').get((req, res) => {
-    res.status(201).json({ message: `Get Contact For ${req.params.id}` });
-});
-
-router.route('/:id').delete((req, res) => {
-    res.status(201).json({ message: `Delete Contact For ${req.params.id}` });
-});
+router.route('/').get(getContact);
+router.route('/').post(createContact);
+router.route('/:id').put(updateContact);
+router.route('/:id').get(getContactById);
+router.route('/:id').delete(deleteContact);
 
 
 module.exports = router;
